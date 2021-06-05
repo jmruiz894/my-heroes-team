04f17626cbb102cd944c2e04b570c37a3e838b22
Jose Miguel Ruiz <jmruiz0894@gmail.com>
2021-06-05 16:03:27 -0300

    ✨ Introducing app component to root file

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './pages/Login/login'
+import './css/app.css'
+
+function App() {
+    return (
+        <div className="App container-fluid">
+            <Login />
+        </div>
+    )
+}
+
+export default App;
