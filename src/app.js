361ebb2c86db46d1b1e0c248310143aba0ce97f5
Jose Miguel Ruiz <jmruiz0894@gmail.com>
2021-06-08 17:11:05 -0300

    ⚡️ Form redirect, "Home" path protection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import Login from './pages/Login/login'
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+
 import './css/app.css'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/login'
 
 function App() {
     return (
-        <div className="App container-fluid">
-            <Login />
+        <div className="App row">
+            <Router>
+                {
+                    localStorage.getItem('token') ? (
+                        <Redirect to="/home" />
+                    ) : (
+                        null
+                    )
+                }
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <ProtectedRoute path="/home" component={Home} />
+                </Switch>
+            </Router>
         </div>
     )
 }
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -56,13 +56,12 @@ export default withFormik({
         email: Yup.string().email('Email is not valid').required('Email is required'),
         password: Yup.string().min(5, "The password must be at least 5 characters long").required('Password is required')
     }),
-    handleSubmit(values, { resetForm }) {
+    handleSubmit(values, { history, resetForm }) {
         axios.post('http://challenge-react.alkemy.org/', values)
             .then(data => {
                 const token = data.data.token
                 localStorage.setItem('token', token)
-                console.log('User logged in');
-                resetForm()
+                history.push('/home')
             }).catch(err => {
                 console.log(err)
                 resetForm()
