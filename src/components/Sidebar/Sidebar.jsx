cbea1f9c988868ce5fac1039202f3d3847737a29
Jose Miguel Ruiz <jmruiz0894@gmail.com>
2021-06-08 18:24:47 -0300

    ✨ Creation of sidebar for navigation

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+//Styles
+import "./Sidebar.css";
+
+const Sidebar = ( props ) => {
+  return (
+    <div
+      className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark"
+      id="sidebar"
+    >
+      <Link
+        to="/home"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+      >
+        <span className="fs-4">My Heroes Team</span>
+      </Link>
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <Link to="#my-team" className="nav-link text-white" aria-current="page">
+            My Team
+          </Link>
+        </li>
+      </ul>
+      <hr />
+    </div>
+  );
+};
+
+export default Sidebar;
